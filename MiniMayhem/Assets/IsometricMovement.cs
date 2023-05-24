9fab708dca651472300449a61379bbe64fd6ffde
Smushy22 <108608110+Smushy22@users.noreply.github.com>
2023-05-24 12:23:56 -0700

    Player Controller

--- MiniMayhem/Assets/IsometricMovement.cs
+++ MiniMayhem/Assets/IsometricMovement.cs
@@ -1,41 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class IsometricMovement : MonoBehaviour
+public class IsometricMovement : MonoBehaviour, Controls.IPlayerActions
+
 {
-    public float speed = 6f;            // The speed that the player will move at.
-    Vector3 movement;                   // The vector to store the direction of the player's movement.
-    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-
-    void Awake()
+    public float speed = 5f;
+    public CharacterController controller;
+    public Vector3 _direction;
+    public void Start()
     {
-        playerRigidbody = GetComponent<RigidBody>();
+        controller = GetComponent<CharacterController>();
+    }
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
     }
 
-
-    void FixedUpdate()
+    public void OnJump(InputAction.CallbackContext context)
     {
-        // Store the input axes.
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-
-        // Move the player around the scene.
-        Move(h, v);
+      //  throw new System.NotImplementedException();
     }
 
-    void Move(float h, float v)
+    public void OnLook(InputAction.CallbackContext context)
     {
-        // Set the movement vector based on the axis input.
-        movement.Set(h, 0f, v);
-
-        // Normalise the movement vector and make it proportional to the speed per second.
-        movement = movement.normalized * speed * Time.deltaTime;
-
-        // Move the player to it's current position plus the movement.
-        playerRigidbody.MovePosition(transform.position + movement);
+        throw new System.NotImplementedException();
     }
 
-    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 readVector = context.ReadValue<Vector2>();
+        Vector3 toConvert = new Vector3(readVector.x, 0, readVector.y);
+        _direction = IsoVectorConvert(toConvert);
+    }
 
+    private Vector3 IsoVectorConvert(Vector3 vector)
+    {
+        Quaternion rotation = Quaternion.Euler(0, 45.0f, 0);
+        Matrix4x4 isoMatrix = Matrix4x4.Rotate(rotation);
+        Vector3 result = isoMatrix.MultiplyPoint3x4(vector);
+        return result;
+    }
+    public void Update()
+    {
+        controller.Move(_direction * speed * Time.deltaTime);
+    }
 }
